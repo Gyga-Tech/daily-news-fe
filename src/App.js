1f553db96da0a4d8b03548b8adee58d750e5f066
hlukman86 <106459212+hlukman86@users.noreply.github.com>
2022-07-21 23:37:43 +0700

    feat: add auth

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ import Category from './page/Category';
 import Articles from './page/Articles';
 import Notification from './page/Notification';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import SignUp from './page/Auth/SignUp';
+import SignIn from './page/Auth/SignIn';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/authenticaion" element={<Auth/>} />
+      <Route path="/authenticaion" element={<Auth/>}>
+        <Route path='signup' element={<SignUp/>}/>
+        <Route path='signin' element={<SignIn/>}/>
+      </Route>
       <Route path="/category" element={<Category/>} />
       <Route path="/articles" element={<Articles />} />
       <Route path="/notification" element={<Notification />} />
--- src/component/Footer/index.js
+++ src/component/Footer/index.js
@@ -1,7 +1,27 @@
+import "./index.css"
 const Footer = () => {
     
     return(
-        <div>Ini Footer</div>
+        <footer className="container-fluid bg-primary">
+           <div className="row mt-5 mx-5 p-5 justify-content-around">
+                <div className="col-lg-3 comunity me-5">
+                    <div className="container d-flex flex-column ">
+                    <p className="font-s">Why News Today</p>
+                    <p className="font-s">Be an author</p>
+                    <p className="font-s">Community</p>
+                    <p className="font-s">FAQ</p>
+                    </div>
+                </div>  
+                <div className="col-lg-3 news-today me-5">
+                    <div className="container d-flex flex-column  ">
+                    <p className="font-m color-white">News Today</p>
+                    <p className="font-s">Jendral Sudirman Street No. 23 Jakarta, Indonesia</p>
+                    <p className="font-s">(621)989898934</p>
+                    <p className="font-s">newstoday@mail.com</p>
+                    </div>
+                </div>  
+            </div>
+        </footer>
     )
 }
 
