4e4b650487976e81e5ead302903072de5761e1c1
zainulmbrk <zainulmbrk.id@gmail.com>
2022-07-24 16:20:14 +0800

    feat: add feature articles

--- src/page/Articles/header.js
+++ src/page/Articles/header.js
@@ -1,14 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './Header.css'
 
 const HeaderSection = () => {
     return (
         <section className="header">
-            <div className="title">
+            <div className="title2">
                 <h1>Start Writing an Article</h1>
                 <p>You can be an author by being active in reading artciles
                     in a month or you can request to be an author if you have been a member for three months</p>
-                <button className="btn btn-primary">Start Writing</button>
+                <Link to="/addarticles">
+
+                    <button className="btn btn-primary">Start Writing</button>
+                </Link>
             </div>
 
         </section>
