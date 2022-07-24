7ceb7b36dc2a6031f10d4565490493aca16e0ed4
trevinkur <mokeyprojek@gmail.com>
2022-07-24 14:47:23 +0700

    feat: get article and fixing articles

--- src/page/Articles/header.js
+++ src/page/Articles/header.js
@@ -4,7 +4,7 @@ import './Header.css'
 const HeaderSection = () => {
     return (
         <section className="header">
-            <div className="title">
+            <div className="title2">
                 <h1>Start Writing an Article</h1>
                 <p>You can be an author by being active in reading artciles
                     in a month or you can request to be an author if you have been a member for three months</p>
