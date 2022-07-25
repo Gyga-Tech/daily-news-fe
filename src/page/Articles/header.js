be3edd3573fa8ed585532d54c89e3f03c48dfdc1
zainulmbrk <zainulmbrk.id@gmail.com>
2022-07-25 22:42:45 +0800

    feat: add header articles

--- src/page/Articles/header.js
+++ src/page/Articles/header.js
@@ -1,18 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import './Header.css'
 
 const HeaderSection = () => {
     return (
         <section className="header">
-            <div className="title2">
+            <div className="title">
                 <h1>Start Writing an Article</h1>
                 <p>You can be an author by being active in reading artciles
                     in a month or you can request to be an author if you have been a member for three months</p>
-                <Link to="/addarticles">
-
-                    <button className="btn btn-primary">Start Writing</button>
-                </Link>
+                <button className="btn btn-primary">Start Writing</button>
             </div>
 
         </section>
